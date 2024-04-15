baa59760dfa397d595dd2868f7df8ce03badebed
iriSVakjyrie <seiryuu2772@gmail.com>
2024-04-15 23:09:25 +0900

    要らない処理を削除,ランキングをHUDMessageで表示するように修正

--- ModEntry.cs
+++ ModEntry.cs
@@ -19,55 +19,14 @@ namespace StardewValleyMoneyRankMod
         /// <param name="helper">Provides simplified APIs for writing mods.</param>
         public override void Entry(IModHelper helper)
         {
-            if (m_MoneyRank == null)
-            {
-                m_MoneyRank = new MoneyRank();
-            }
-            
-            helper.Events.Input.ButtonPressed += this.OnButtonPressed;
             helper.Events.GameLoop.DayStarted += this.OnDayStarted;
-            helper.Events.Multiplayer.PeerConnected += this.OnConnectPlayer;
-            helper.Events.Multiplayer.PeerDisconnected += this.OnDisConnectPlayer;
-
-            
         }
 
-        private void OnDisConnectPlayer(object? sender, PeerDisconnectedEventArgs e)
+        private void OnDayStarted(object? sender, DayStartedEventArgs e)
         {
-            m_MoneyRank.RemovePlayer(e.Peer);
-        }
-
-        private void OnConnectPlayer(object? sender, PeerConnectedEventArgs e)
-        {
-            m_MoneyRank.AddPlayer(e.Peer);
-        }
-
-
-        /*********
-        ** Private methods
-        *********/
-        /// <summary>Raised after the player presses a button on the keyboard, controller, or mouse.</summary>
-        /// <param name="sender">The event sender.</param>
-        /// <param name="e">The event data.</param>
-        private void OnButtonPressed(object? sender, ButtonPressedEventArgs e)
-        {
-            // ignore if player hasn't loaded a save yet
-            if (!Context.IsWorldReady)
-                return;
-
-
-            if (e.Button == SButton.F2)
-            {
-                
-            }
-            
-            // print button presses to the console window
-            this.Monitor.Log($"{Game1.player.Name} pressed {e.Button}.", LogLevel.Debug);
-        }
-
-        private void OnDayStarted(object? sender,DayStartedEventArgs e)
-        {
-            
+            this.Monitor.Log($"Day Starded", LogLevel.Debug);
+            if(!m_MoneyRank.IsInit) m_MoneyRank.Init();
+            m_MoneyRank.ShowRankingUI(this.Monitor);
         }
     }
 }
--- MoneyRank.cs
+++ MoneyRank.cs
@@ -1,22 +1,86 @@
 ﻿using StardewModdingAPI;
+using StardewValley;
+using StardewValley.Menus;
 
-namespace StardewValleyMoneyRankMod;
 
-public class MoneyRank
+namespace StardewValleyMoneyRankMod
 {
-    private List<long> m_ConnectingPlayerIDs = new List<long>();
-    public MoneyRank()
+    public class MoneyRank
     {
-        
-    }
+        private List<MoneyRankData> m_Ranks = new List<MoneyRankData>();
 
-    public void AddPlayer(IMultiplayerPeer peer)
-    {
-        m_ConnectingPlayerIDs.Add(peer.PlayerID);
-    }
+        private int showRankIn =5;
+        public bool IsInit
+        {
+            get => m_Ranks.Count == 0 ? false : true;
+        }
+        public MoneyRank()
+        {
+            
+        }
 
-    public void RemovePlayer(IMultiplayerPeer peer)
-    {
-        m_ConnectingPlayerIDs.Remove(peer.PlayerID);
+        public void Init()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+        {
+            int count = Game1.getAllFarmers().Count();
+            for (int i = 0; i < count; i++)
+            {
+                m_Ranks.Add(new MoneyRankData("",0));
+            }
+        }
+
+        public void ShowRankingUI(IMonitor debug)
+        {
+            IOrderedEnumerable<MoneyRankData> ranking = GetRanking();
+            string message = "-稼いだ金額ランキング-\n";
+            /*
+            debug.Log("-稼いだ金額ランキング-",LogLevel.Debug);
+            foreach (var rank in ranking.Select((value,index)=>(value,index)))
+            {
+                debug.Log($"{rank.index + 1}位 | {rank.value.Name} | {rank.value.TotalMoney:N0} G |" ,LogLevel.Debug);
+            }*/
+
+            for (int i = 0; i < showRankIn; i++)
+            {
+                if (i >= ranking.Count()) continue;
+                MoneyRankData data = ranking.ElementAt(i);
+
+                message += $"{i + 1}位{data.Name}\n";
+                message += $"{data.TotalMoney:N0}G\n";
+            }
+            /*
+            foreach (var rank in ranking.Select((value,index)=>(value,index)))
+            {
+                message += $"{rank.index + 1}位 | {rank.value.Name} | {rank.value.TotalMoney:N0} G |\n";
+            }
+            */
+            string parseMessage = Game1.parseText(message, Game1.smallFont, Game1.viewport.Width);
+            HUDMessage hud = new HUDMessage(parseMessage)
+            {
+                noIcon = true,
+                timeLeft = 5250f
+            };
+            Game1.addHUDMessage(hud);
+        }
+
+        private IOrderedEnumerable<MoneyRankData> GetRanking()
+        {
+            IEnumerable<Farmer> farmers = Game1.getAllFarmers();
+
+            foreach (var farmer in farmers.Select((value,index)=>(value,index)))
+            {
+                if (farmer.index > m_Ranks.Count)
+                {
+                    m_Ranks.Add(new MoneyRankData("",0));
+                };
+                string name = farmer.value.Name;
+                uint totalMoney = farmer.value.stats.IndividualMoneyEarned;
+                m_Ranks[farmer.index].Name = name;
+                m_Ranks[farmer.index].TotalMoney = totalMoney;
+            }
+
+            return m_Ranks.OrderByDescending(f => f.TotalMoney);
+            
+        }
     }
+    
 }
